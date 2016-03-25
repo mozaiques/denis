161564f9e4bed6fcea10fd31fd46d3bcec56ef58
Bastien Gandouet <bastien@mozaiqu.es>
2016-03-25 23:51:00 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ description = (
 
 setup(
     name='denis',
-    version='0.0.4',
+    version='0.0.5',
     description=description,
     url='https://github.com/mozaiques/denis',
     author='Bastien Gandouet',
